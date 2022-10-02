b4e6c6905edcade29e3e917ae4860a3e794b5b1f
青柠 <1405784613@qq.com>
2022-10-02 11:01:29 +0800

    feat: login static page development

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': 'off'
+    'space-before-function-paren': 'off',
+    // 关闭组件命名规则
+    'vue/multi-word-component-names': 'off'
   }
 }
